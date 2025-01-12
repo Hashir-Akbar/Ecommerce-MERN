3eec8612e338c05f28f37cb6bedeb15a07c69379
Hashir Akbar <hashirbusinessinfo@gmail.com>
2025-01-13 02:43:17 +0500

    basic setup #3

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
+import notFound from "./middleware/404.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+app.use(notFound);
 
 app.listen(PORT, () => {
   console.log("Server Running at port " + PORT);
