d4a6e6e8b0ef176cbdbdd378da0ac37920cbc21a
Hashir Akbar <hashirbusinessinfo@gmail.com>
2025-02-14 23:45:58 +0500

    converted server to ts

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-
 app.get("/", (req, res) => {
   res.send("hello world");
 });
--- Server/src/middleware/404.js
+++
@@ -1,5 +0,0 @@
-const notFound = (req, res) => {
-  res.status(404).send("404 Not Found");
-};
-
-export default notFound;
